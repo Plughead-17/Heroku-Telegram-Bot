fbdf613c39dac86268ef602e4fc542e87f692bfc
Plughead-17 <plughead@list.ru>
2021-08-22 16:21:41 +0300

    make it better

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def Test():
 #    items = parser.get_content(page)
     bot.sendmessagebot("hi")
 ##########################################################################
-@bot.bot_1.message_handler(commands = ["test_key"])
+
+######################
+#   start
+@bot.bot_1.message_handler(commands = ["/start_key"])
 def start(message):
     bot.bot_1.reply_to(message, "hello, " + message.from_user.first_name)
 
+######################
+#
 @bot.bot_1.message_handler(func = lambda message: True, content_types = ["text"])
 def echo(message):
     bot.bot_1.reply_to(message, message.text)
 
+######################
+#
 @server.route('/' + misc.TELEGRAM_TOKEN, methods = ["POST"])
 def get_message():
     string = request.get_data().decode("utf-8")
@@ -28,16 +35,17 @@ def get_message():
     bot.bot_1.process_new_updates([update])
     return '|', 200
 
+######################
+#
 @server.route('/')
 def webhook():
     bot.bot_1.remove_webhook()
-    bot.bot_1.send_message(misc.URL_APP)
+    bot.bot_1.send_wbhook(url = misc.URL_APP)
     return '|', 200
 ##########################################################################
 #       Main
 def main():
     print("Start APP!")
-
     server.run(host = "0.0.0.0", port = int(os.environ.get("PORT", 5000)))
 #    bot.RunRunFastUCan()
 
